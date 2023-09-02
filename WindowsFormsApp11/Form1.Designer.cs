eafb9c18a5c2fea73383a69b2e250b1612fe9567
User <User@DESKTOP-D1KEAHC>
2023-09-02 22:54:28 +0530

    Add project files.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit = new System.Windows.Forms.Button();
+            this.Newbtn = new System.Windows.Forms.Button();
+            this.Evaluatebtn = new System.Windows.Forms.Button();
+            this.ntxt = new System.Windows.Forms.TextBox();
+            this.mtxt = new System.Windows.Forms.TextBox();
+            this.dtxt = new System.Windows.Forms.TextBox();
+            this.btxt = new System.Windows.Forms.TextBox();
+            this.ctxt = new System.Windows.Forms.TextBox();
+            this.atxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nLbl = new System.Windows.Forms.Label();
+            this.mLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(541, 307);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 67;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Newbtn
+            // 
+            this.Newbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Newbtn.Location = new System.Drawing.Point(376, 307);
+            this.Newbtn.Name = "Newbtn";
+            this.Newbtn.Size = new System.Drawing.Size(75, 23);
+            this.Newbtn.TabIndex = 66;
+            this.Newbtn.Text = "New";
+            this.Newbtn.UseVisualStyleBackColor = true;
+            this.Newbtn.Click += new System.EventHandler(this.Newbtn_Click);
+            // 
+            // Evaluatebtn
+            // 
+            this.Evaluatebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Evaluatebtn.Location = new System.Drawing.Point(209, 307);
+            this.Evaluatebtn.Name = "Evaluatebtn";
+            this.Evaluatebtn.Size = new System.Drawing.Size(75, 23);
+            this.Evaluatebtn.TabIndex = 65;
+            this.Evaluatebtn.Text = "Evaluate";
+            this.Evaluatebtn.UseVisualStyleBackColor = true;
+            this.Evaluatebtn.Click += new System.EventHandler(this.Evaluatebtn_Click);
+            // 
+            // ntxt
+            // 
+            this.ntxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ntxt.Location = new System.Drawing.Point(530, 253);
+            this.ntxt.Name = "ntxt";
+            this.ntxt.Size = new System.Drawing.Size(100, 26);
+            this.ntxt.TabIndex = 64;
+            // 
+            // mtxt
+            // 
+            this.mtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtxt.Location = new System.Drawing.Point(530, 203);
+            this.mtxt.Name = "mtxt";
+            this.mtxt.Size = new System.Drawing.Size(100, 26);
+            this.mtxt.TabIndex = 63;
+            // 
+            // dtxt
+            // 
+            this.dtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtxt.Location = new System.Drawing.Point(362, 253);
+            this.dtxt.Name = "dtxt";
+            this.dtxt.Size = new System.Drawing.Size(100, 26);
+            this.dtxt.TabIndex = 62;
+            // 
+            // btxt
+            // 
+            this.btxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btxt.Location = new System.Drawing.Point(362, 203);
+            this.btxt.Name = "btxt";
+            this.btxt.Size = new System.Drawing.Size(100, 26);
+            this.btxt.TabIndex = 61;
+            // 
+            // ctxt
+            // 
+            this.ctxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctxt.Location = new System.Drawing.Point(209, 253);
+            this.ctxt.Name = "ctxt";
+            this.ctxt.Size = new System.Drawing.Size(100, 26);
+            this.ctxt.TabIndex = 60;
+            // 
+            // atxt
+            // 
+            this.atxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atxt.Location = new System.Drawing.Point(209, 203);
+            this.atxt.Name = "atxt";
+            this.atxt.Size = new System.Drawing.Size(100, 26);
+            this.atxt.TabIndex = 59;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(505, 260);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 20);
+            this.label8.TabIndex = 58;
+            this.label8.Text = "n=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(503, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 20);
+            this.label7.TabIndex = 57;
+            this.label7.Text = "m=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(337, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 20);
+            this.label6.TabIndex = 56;
+            this.label6.Text = "d=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(337, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "b=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(170, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 20);
+            this.label4.TabIndex = 54;
+            this.label4.Text = "c=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(170, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 20);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "a=";
+            // 
+            // nLbl
+            // 
+            this.nLbl.AutoSize = true;
+            this.nLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nLbl.Location = new System.Drawing.Point(386, 163);
+            this.nLbl.Name = "nLbl";
+            this.nLbl.Size = new System.Drawing.Size(67, 20);
+            this.nLbl.TabIndex = 52;
+            this.nLbl.Text = "cx+dy=n";
+            // 
+            // mLbl
+            // 
+            this.mLbl.AutoSize = true;
+            this.mLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mLbl.Location = new System.Drawing.Point(386, 121);
+            this.mLbl.Name = "mLbl";
+            this.mLbl.Size = new System.Drawing.Size(72, 20);
+            this.mLbl.TabIndex = 51;
+            this.mLbl.Text = "ax+bx=m";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Newbtn);
+            this.Controls.Add(this.Evaluatebtn);
+            this.Controls.Add(this.ntxt);
+            this.Controls.Add(this.mtxt);
+            this.Controls.Add(this.dtxt);
+            this.Controls.Add(this.btxt);
+            this.Controls.Add(this.ctxt);
+            this.Controls.Add(this.atxt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nLbl);
+            this.Controls.Add(this.mLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Newbtn;
+        private System.Windows.Forms.Button Evaluatebtn;
+        private System.Windows.Forms.TextBox ntxt;
+        private System.Windows.Forms.TextBox mtxt;
+        private System.Windows.Forms.TextBox dtxt;
+        private System.Windows.Forms.TextBox btxt;
+        private System.Windows.Forms.TextBox ctxt;
+        private System.Windows.Forms.TextBox atxt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label nLbl;
+        private System.Windows.Forms.Label mLbl;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Evaluatebtn_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(atxt.Text);
+            int b = int.Parse(btxt.Text);
+            int m = int.Parse(mtxt.Text);
+            int c = int.Parse(ctxt.Text);
+            int d = int.Parse(dtxt.Text);
+            int n = int.Parse(ntxt.Text);
+            int y = ((m * c - n * a) / (c * b - a * d));
+            int x = ((d * m - b * n) / (a * d - b * c));
+            mLbl.Text = "X=" + x.ToString();
+            nLbl.Text = "Y=" + y.ToString();
+        }
+
+        private void Newbtn_Click(object sender, EventArgs e)
+        {
+            atxt.Clear();
+            btxt.Clear();
+            mtxt.Clear();
+            ctxt.Clear();
+            dtxt.Clear();
+            ntxt.Clear();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
+
+          
